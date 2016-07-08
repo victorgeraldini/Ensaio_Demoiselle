abf96668dcbcce04fa9afa30aa9f22afa0467062
Victor Rezende <victorgeraldini@gmail.com>
2016-07-08 10:52:27 -0300

    commit

---
+++ AppletCustomizada/src/main/java/br/gov/frameworkdemoiselle/certificate/sample/applet/Teste.java
@@ -0,0 +1,31 @@
+package br.gov.frameworkdemoiselle.certificate.sample.applet;
+
+import java.security.KeyStore;
+
+import javax.swing.JApplet;
+import javax.swing.JButton;
+
+import br.gov.frameworkdemoiselle.certificate.applet.view.JKeyStoreDialog;
+import br.gov.frameworkdemoiselle.certificate.applet.view.JKeyStorePanel;
+import br.gov.frameworkdemoiselle.certificate.applet.view.JPanelApplet;
+
+public class Teste extends JApplet
+{
+	public void init()
+	{
+//		JKeyStorePanel keyStorePanel = new JKeyStorePanel();
+//		this.getContentPane().add(keyStorePanel);
+		JButton confirm = new JButton("confirmar");
+		this.getContentPane().add(confirm);
+		
+		//if(confirm.)
+		
+//		JKeyStoreDialog dialog = new JKeyStoreDialog();
+//		dialog.setModal(true);
+//		dialog.setVisible(true);
+//
+//		KeyStore keystoreDialog = dialog.getKeyStore();
+		
+//		JPanelApplet applet = new JPanelApplet();
+	}
+}
